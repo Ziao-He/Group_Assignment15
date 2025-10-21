4c9b47321d63a4da5860879e65aaad190a886cbb
bogdog2b <120654221@qq.com>
2025-10-20 23:35:55 -0400

    Complete CouseGrade Class

---
+++ University System/src/Business/Course/CourseGrade.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Course;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CourseGrade {
+     private Course course;
+    private String term; 
+    private String grade; 
+
+    public CourseGrade(Course course, String term, String grade){
+        this.course = course;
+        this.term = term;
+        this.grade = grade;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public double getGradePoint(){
+        if (grade == null) return 0.0;
+        switch(grade){
+            case "A": return 4.0;
+            case "A-": return 3.7;
+            case "B+": return 3.3;
+            case "B": return 3.0;
+            case "B-": return 2.7;
+            case "C+": return 2.3;
+            case "C": return 2.0;
+            case "C-": return 1.7;
+            default: return 0.0; 
+        }
+    }
+}
